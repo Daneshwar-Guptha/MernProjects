80fea0af6845b497f079eb1fa0cc979998e9989a
Daneshwar <saiguptha663@gmail.com>
2025-10-22 23:23:14 +0530

    changes on

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const DBConnection = require('./config/DBConnection');
 const User = require('./model/UserSchema')
 const cors = require('cors');
 const cookie = require("cookie");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json())
@@ -12,12 +14,14 @@ app.post('/signup', async (req, res) => {
     try {
         const { username, email, password } = req.body;
         const userData = await User.findOne({ email });
+
         if (userData) {
-           return res.status(400).json({ message: "User already exists with this email" });
+            return res.status(400).json({ message: "User already exists with this email" });
         }
 
         else {
-            const newUser = new User({username,email,password });
+            const bcryptPassword = await bcrypt.hash(password, 10)
+            const newUser = new User({ username, email, password: bcryptPassword });
             await newUser.save();
             res.status(201).send(newUser)
         }
@@ -26,6 +30,34 @@ app.post('/signup', async (req, res) => {
         res.send(error.message);
 
 
+
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const { password, username, email } = req.body;
+        const Data = await User.findOne({ email });
+        if (!Data) {
+            res.send("please first register");
+        }
+        else {
+            const checkPassword = await bcrypt.compare(password, Data.password);
+            if (!checkPassword) {
+                res.status(401).send("Invalid password");
+            }
+            else {
+                console.log(checkPassword);
+                const token = jwt.sign({ Data }, 'UserSchema');
+                res.cookie("token", token)
+                res.send(Data);
+            }
+
+        }
+
+    }
+    catch (error) {
+        res.status(400).send(error.message)
 
     }
 })
